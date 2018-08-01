8c7b8a705c8c10b777ffe4e188e3e0b325ba6ffd
Kristian Giglia <krismania1@gmail.com>
2018-08-01 12:53:02 +1000

    Add index template with map display

--- project/src/main/java/Main.java
+++ project/src/main/java/Main.java
@@ -1,11 +1,39 @@
 import static spark.Spark.get;
 import static spark.Spark.port;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
 	port(4567);
-	get("/hello", (req, res) -> "Hello World");
+
+	Properties prop = new Properties();
+
+	// Load properties
+	try (InputStream propFile = new FileInputStream("config.properties")) {
+	    prop.load(propFile);
+	} catch (FileNotFoundException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
+	String mapsApiKey = prop.getProperty("mapsApiKey");
+
+	get("/", (req, res) -> {
+	    Map<String, Object> model = new HashMap<>();
+	    model.put("mapsApiKey", mapsApiKey);
+	    return Util.render(model, "index");
+	});
     }
 
 }

860d6eb7b412a14ad5131258256041445a1f41b9
Kristian Giglia <krismania1@gmail.com>
2018-08-05 02:01:42 +1000

    Add js to access api in front end

--- project/src/main/java/controllers/ApiController.java
+++ project/src/main/java/controllers/ApiController.java
@@ -8,7 +8,7 @@ public class ApiController {
 
 	get("/api/vehicles", (req, res) -> {
 	    res.type("application/json");
-	    return "[{\"registration\":\"ABC123\",\"make\":\"Toyota\",\"model\":\"Corolla\",\"year\":2014,\"color\":\"Blue\",\"position\":{\"lat\":-37.808401,\"lon\":144.956159},\"available\":True}]";
+	    return "[{\"registration\":\"ABC123\",\"make\":\"Toyota\",\"model\":\"Corolla\",\"year\":2014,\"color\":\"Blue\",\"position\":{\"lat\":-37.808401,\"lon\":144.956159},\"available\":true}]";
 	});
 
     }
--- project/src/main/resources/static/js/main.js
+++ project/src/main/resources/static/js/main.js
@@ -5,5 +5,20 @@ function initMap() {
 			center: {lat: pos.coords.latitude, lng: pos.coords.longitude},
 			zoom: 12
 		});
+		
+		var request = new Request('/api/vehicles');
+		fetch(request)
+		.then(res => res.json())
+		.then(json => {
+			var marker = new google.maps.Marker({
+				position: {
+					lat: json[0].position.lat,
+					lng: json[0].position.lon
+				},
+				map: map,
+				title: json[0].registration
+			});
+			console.log(marker);
+		});
 	});
 }

70a45fe9eb8bf89cb42a76b19413b3df463d397f
Kristian Giglia <krismania1@gmail.com>
2018-08-05 12:28:51 +1000

    Add custom vehicle pins

--- project/src/main/resources/static/js/main.js
+++ project/src/main/resources/static/js/main.js
@@ -10,13 +10,27 @@ function initMap() {
 		fetch(request)
 		.then(res => res.json())
 		.then(json => {
+			var iconAvailable = {
+				url: '/img/vehicle-pin-available.png',
+				size: new google.maps.Size(40, 40),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(40, 40)
+			};
+			var iconUnavailable = {
+				url: '/img/vehicle-pin-unavailable.png',
+				size: new google.maps.Size(40, 40),
+				origin: new google.maps.Point(0, 0),
+				anchor: new google.maps.Point(20, 40)
+			};
 			for (var i = 0; i < json.length; i++) {
+				console.log(json[i]);
 				new google.maps.Marker({
 					position: json[i].position,
 					map: map,
+					icon: json[i].available ? iconAvailable : iconUnavailable,
 					title: json[i].registration
-				})
-			}
+				});
+			};
 		});
 	});
 }

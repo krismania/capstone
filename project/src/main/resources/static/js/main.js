06c85ad4e6223a1a562f2e8c7d29aec66e3d7e35
Kristian Giglia <krismania1@gmail.com>
2018-08-21 17:16:44 +1000

    Add sign out function

--- project/src/main/resources/static/js/main.js
+++ project/src/main/resources/static/js/main.js
@@ -13,20 +13,12 @@ function onSuccess(googleUser) {
     console.log("ID Token: " + id_token);
     alert(id_token);
   }
-function onFailure(error) {
-    console.log(error);
-}
-  
-function renderButton() {
-    gapi.signin2.render('my-signin2', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSuccess,
-      'onfailure': onFailure
-    });
+
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+	});
 }
 
 function initGeolocate() {

8b6e8b571dabdb64498f35abf9faaefe230865ef
Kristian Giglia <krismania1@gmail.com>
2018-08-22 19:34:48 +1000

    Create js api method `getNearby()`

--- project/src/main/resources/static/js/main.js
+++ project/src/main/resources/static/js/main.js
@@ -53,7 +53,7 @@ function geolocateHandler(e) {
 
 function nearbyHandler(e) {
 	var pos = geoMarker.marker.getPosition()
-	nearbyCars(pos.lat, pos.lng);
+	nearbyCars(pos);
 }
 
 function initMap() {
@@ -182,29 +182,23 @@ function submitBooking(vehicle) {
 	sidepane.append(view.bookingConfirmed());
 }
 
-function nearbyCars(lat, lng) {
-	// fetch nearby cars json
-	// this is a simulated response
-	var nearby = [
-		{registration: "QRB990", description: "BMW 325i (2003)", colour: "Black", distance: "500 m"},
-		{registration: "JTD955", description: "Holden Commodore (2005)", colour: "Grey", distance: "800 m"},
-		{registration: "FOK356", description: "Holden Barina (2017)", colour: "White", distance: "1.2 km"},
-		{registration: "QOP299", description: "Kia Rio (2013)", colour: "Pink", distance: "1.8 km"},
-		{registration: "YODUDE", description: "Nissan Skyline (2010)", colour: "Black", distance: "2.1 km"},
-	];
-	// show the response
-	sidepane.clear();
-	sidepane.appendHeader("NEARBY CARS");
-	for (var i = 0; i < nearby.length; i++) {
-		let vehicle = nearby[i];
-		console.log(vehicle);
-		var nearbyVehicle = view.nearbyVehicle(vehicle, function(e) {
-			e.preventDefault();
-			bookingForm(vehicle);
-		});
-		sidepane.append(nearbyVehicle);
-	}
-	sidepane.open();
+function nearbyCars(pos) {
+	// fetch nearby cars
+	rebu.getNearby(pos, function(nearby) {
+		// show the response
+		sidepane.clear();
+		sidepane.appendHeader("NEARBY CARS");
+		for (var i = 0; i < nearby.length; i++) {
+			let vehicle = nearby[i];
+			console.log(vehicle);
+			var nearbyVehicle = view.nearbyVehicle(vehicle, function(e) {
+				e.preventDefault();
+				bookingForm(vehicle);
+			});
+			sidepane.append(nearbyVehicle);
+		}
+		sidepane.open();
+	});
 }
 
 // initialize sidepane
--- project/src/main/resources/static/js/rebu.js
+++ project/src/main/resources/static/js/rebu.js
@@ -22,6 +22,19 @@ rebu = (function() {
 				console.log(vehicles);
 				callback(vehicles);
 			});
+		},
+		
+		getNearby: function(pos, callback) {
+			console.log("[api] getting all vehicles");
+			// TODO: link this route
+			var vehicles = [
+				{registration: "QRB990", description: "BMW 325i (2003)", colour: "Black", distance: "500 m"},
+				{registration: "JTD955", description: "Holden Commodore (2005)", colour: "Grey", distance: "800 m"},
+				{registration: "FOK356", description: "Holden Barina (2017)", colour: "White", distance: "1.2 km"},
+				{registration: "QOP299", description: "Kia Rio (2013)", colour: "Pink", distance: "1.8 km"},
+				{registration: "YODUDE", description: "Nissan Skyline (2010)", colour: "Black", distance: "2.1 km"},
+			];
+			callback(vehicles);
 		}
 	
 	}

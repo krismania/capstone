e1a31ea38ca6fc8e16570d5fe2a6ca4574a87133
Kristian Giglia <krismania1@gmail.com>
2018-08-24 22:21:42 +1000

    Add location to vehicle schema

--- project/src/main/java/model/Database.java
+++ project/src/main/java/model/Database.java
@@ -73,7 +73,7 @@ public class Database implements Closeable {
 	String vehiclesSql = "CREATE TABLE IF NOT EXISTS `vehicles` (" + "`registration` VARCHAR(10) NOT NULL, "
 		+ "`make` VARCHAR(50) NOT NULL, " + "`model` VARCHAR(50) NOT NULL, "
 		+ "`year` SMALLINT UNSIGNED NOT NULL, " + "`colour` VARCHAR(50) NOT NULL, "
-		+ "PRIMARY KEY (`registration`));";
+		+ "location POINT NOT NULL, " + "PRIMARY KEY (`registration`));";
 
 	String bookingsSql = "CREATE TABLE IF NOT EXISTS `bookings` (" + "`id` INT NOT NULL AUTO_INCREMENT, "
 		+ "`timestamp` DATETIME NOT NULL, " + "`registration` VARCHAR(10) NOT NULL, "

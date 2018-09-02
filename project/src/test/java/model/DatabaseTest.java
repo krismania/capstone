18c54e7453a4da8bfa1a64f9113a0921d7cf9f1a
Aaron Tran <s3540620@student.rmit.edu.au>
2018-09-03 09:59:15 +1000

    Added more tests

--- project/src/test/java/model/DatabaseTest.java
+++ project/src/test/java/model/DatabaseTest.java
@@ -21,4 +21,39 @@ public class DatabaseTest {
 	System.out.println(vehicles.size());
 	assertEquals(expected, vehicles.size());
     }
+
+    @Test
+    void testVehicleArray() {
+
+	Vehicle veh = new Vehicle("ABC123", "Toyota", "Corolla", 2014, "Blue", new Position(-37.808401, 144.956159));
+	List<Vehicle> vehicles = new ArrayList<Vehicle>();
+	vehicles = this.database.getVehicles();
+
+	assertEquals(veh.getMake(), vehicles.get(0).getMake());
+    }
+
+    @Test
+    void testNearbyArrayReturns() {
+
+	Position pos = new Position(-37.808401, 144.956159);
+	List<Vehicle> vehicles = new ArrayList<Vehicle>();
+	vehicles = this.database.getVehicles();
+	List<NearbyVehicle> vehiclesN = new ArrayList<NearbyVehicle>();
+	vehiclesN = this.database.getNearbyVehicles(pos);
+
+	assertEquals(vehiclesN.get(0).getDistance(), 0);
+    }
+
+    @Test
+    void testNearbyFirstCar() {
+
+	Position pos = new Position(-37.808401, 144.956159);
+	List<Vehicle> vehicles = new ArrayList<Vehicle>();
+	vehicles = this.database.getVehicles();
+	List<NearbyVehicle> vehiclesN = new ArrayList<NearbyVehicle>();
+	vehiclesN = this.database.getNearbyVehicles(pos);
+
+	assertEquals(vehiclesN.get(0).getDescription(), vehicles.get(0).getDescription());
+    }
+
 }

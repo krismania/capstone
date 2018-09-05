137727aa4f8dafc0432500dac1717ee44db03627
Kristian Giglia <krismania1@gmail.com>
2018-09-06 00:10:43 +1000

    Add route & template for account page

--- project/src/main/java/controllers/UiController.java
+++ project/src/main/java/controllers/UiController.java
@@ -14,14 +14,20 @@ public class UiController {
 
     public UiController(String mapsApiKey, String googleClientId) {
 
+	// create model with API keys
+	Map<String, Object> model = new HashMap<>();
+	model.put("mapsApiKey", mapsApiKey);
+	model.put("googleClientId", googleClientId);
+
 	@SuppressWarnings("unused")
 	final Logger logger = LoggerFactory.getLogger(UiController.class);
 
 	get("/", (req, res) -> {
-	    Map<String, Object> model = new HashMap<>();
-	    model.put("mapsApiKey", mapsApiKey);
-	    model.put("googleClientId", googleClientId);
 	    return Util.render(model, "index");
+	});
+
+	get("/account", (req, res) -> {
+	    return Util.render(model, "account");
 	});
 
     }

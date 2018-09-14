d665029187008e2e2d35837eed3eb803d1e2eee6
Aaron Tran <s3540620@student.rmit.edu.au>
2018-09-14 20:45:28 +1000

    change to post

--- project/src/main/java/controllers/ApiController.java
+++ project/src/main/java/controllers/ApiController.java
@@ -116,7 +116,7 @@ public class ApiController {
 	    return new Gson().toJson(bookings);
 	});
 
-	get("/credit/insert", (req, res) -> {
+	post("/credit/insert", (req, res) -> {
 	    res.type("application/json");
 	    String clientId = req.session().attribute("clientId");
 

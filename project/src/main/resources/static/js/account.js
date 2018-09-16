714268eda1d01e33f00dd59d9317453798af370a
Martin <cmaartinchan@gmail.com>
2018-09-16 19:24:10 +1000

    Re-ordered Recent Booking List

--- project/src/main/resources/static/js/account.js
+++ project/src/main/resources/static/js/account.js
@@ -4,7 +4,7 @@ document.addEventListener("login", function() {
 	rebu.getBookings(user, function(bookings) {
 		console.log(bookings);
 		var bookingsDiv = document.getElementById("prev-bookings");
-		for (var i = 0; i < bookings.length; i++) {
+		for (var i = bookings.length-1; i >=0 ; i--) {
 			// TODO: server may need to send this as a seperate field
 			bookings[i].vehicle.description = bookings[i].vehicle.make + " "
 				+ bookings[i].vehicle.model

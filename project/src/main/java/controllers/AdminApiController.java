2055b72f840b8b7c4dda561455c960c2765a4968
Kristian Giglia <krismania1@gmail.com>
2018-10-03 09:10:11 +1000

    Move admin auth to cover UI

--- project/src/main/java/controllers/AdminApiController.java
+++ project/src/main/java/controllers/AdminApiController.java
@@ -1,9 +1,7 @@
 package controllers;
 
-import static spark.Spark.before;
 import static spark.Spark.delete;
 import static spark.Spark.get;
-import static spark.Spark.halt;
 import static spark.Spark.path;
 import static spark.Spark.post;
 import static spark.Spark.put;
@@ -43,21 +41,6 @@ public class AdminApiController {
     public AdminApiController() {
 
 	final Logger logger = LoggerFactory.getLogger(AdminApiController.class);
-
-	// authenticate admin users
-	before("/*", (req, res) -> {
-	    logger.info("Admin API Request: " + req.uri());
-	    if (req.session(false) == null) {
-		logger.info("User is not logged in");
-		halt(401);
-	    }
-	    boolean isAdmin = req.session().attribute("isAdmin");
-	    if (!isAdmin) {
-		logger.info("User is NOT an administrator");
-		halt(403);
-	    }
-	    logger.info("User is an administrator");
-	});
 
 	// create a new vehicle
 	post("/vehicles", (req, res) -> {

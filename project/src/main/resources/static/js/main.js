977d37863877c53b03ae6716be5b809a98ec7fd0
Kristian Giglia <krismania1@gmail.com>
2018-10-06 15:12:29 +1000

    Integrate paypal button with UI

--- project/src/main/resources/static/js/main.js
+++ project/src/main/resources/static/js/main.js
@@ -334,6 +334,7 @@ function endBooking(booking) {
 	rebu.endCurrentBooking(function(success) {
 		if (success) {
 			if (map != null) {
+				paypalPrompt(booking);
 				removeCurrentBooking();
 				rebu.getVehicles(displayVehicles);
 			} else {
@@ -380,6 +381,52 @@ function removeCurrentBooking() {
 			document.body.removeChild(currentBookingCard);
 		}, 200);
 	}
+}
+
+// prompt the user with the sidepane to pay via paypal
+function paypalPrompt(booking) {
+	sidepane.clear();
+	sidepane.appendHeader("PAYMENT");
+	sidepane.append(view.payment(booking));
+	// render paypal button
+	paypal.Button.render({
+	    env: 'sandbox', // sandbox | production
+	    style: {
+	        label: 'pay',
+	        tagline: false,
+	        size:  'medium',    // small | medium | large | responsive
+	        shape: 'rect',     // pill | rect
+	        color: 'blue'      // gold | blue | silver | black
+	    },
+	    client: {
+	        sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+	        production: '<insert production client id>'
+	    },
+	    payment: function(data, actions) {
+	        return actions.payment.create({
+	            payment: {
+	                transactions: [
+	                    {
+	                        amount: { total: '0.01', currency: 'AUD' }
+	                    }
+	                ]
+	            }
+	        });
+	    },
+	    onAuthorize: function(data, actions) {
+	        return actions.payment.execute().then(function() {
+		    	sidepane.clear();
+		    	sidepane.appendHeader("PAYMENT");
+		    	sidepane.append(view.paymentConfirmation(true));
+	        });
+	    },
+	    onError: function(err) {
+	    	sidepane.clear();
+	    	sidepane.appendHeader("PAYMENT");
+	    	sidepane.append(view.paymentConfirmation(false));
+	    }
+	}, '#paypal-button-container');
+	sidepane.open();
 }
 
 // initialize sidepane
--- project/src/main/resources/static/js/rebu.js
+++ project/src/main/resources/static/js/rebu.js
@@ -163,44 +163,6 @@ rebu = (function() {
 			
 			var request = new Request('/api/bookings/end');
 			
-
-			paypal.Button.render({
-
-			    env: 'sandbox', // sandbox | production
-
-			    style: {
-			        label: 'checkout',
-			        size:  'small',    // small | medium | large | responsive
-			        shape: 'pill',     // pill | rect
-			        color: 'blue'      // gold | blue | silver | black
-			    },
-
-			    client: {
-			        sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
-			        production: '<insert production client id>'
-			    },
-
-			    payment: function(data, actions) {
-			        return actions.payment.create({
-			            payment: {
-			                transactions: [
-			                    {
-			                        amount: { total: '0.01', currency: 'AUD' }
-			                    }
-			                ]
-			            }
-			        });
-			    },
-
-			    onAuthorize: function(data, actions) {
-			        return actions.payment.execute().then(function() {
-			            window.alert('Payment Complete!');
-			        });
-			    }
-
-			}, '#paypal-button-container');
-
-			
 			fetch(request)
 			.then(res => {
 				if (res.status == 200) {
